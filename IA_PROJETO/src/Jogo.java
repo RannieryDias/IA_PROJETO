917c8bed758e563e06544c565f9334bbdfebee65
Ranniery <Ranniery@RANNIERY-PC>
2018-07-11 19:40:07 -0300

    Criação da classe Jogo

---
+++ IA_PROJETO/src/Jogo.java
@@ -0,0 +1,133 @@
+
+public class Jogo {
+	private int recommendationcount; 
+	private boolean categorysingleplayer;
+	private boolean categorymultiplayer;
+	private boolean categorycoop;
+	private boolean categorymmo;
+	private boolean categoryvrsupport;
+	private boolean genreisindie;
+	private boolean genreisaction;
+	private boolean genreisadventure;
+	private boolean genreiscasual;
+	private boolean genreisstrategy;
+	private boolean genreisrpg;
+	private boolean genreissimulation;
+	private boolean genreissports;
+	private boolean genreisracing;
+	private boolean genreismassivelymultiplayer;
+	private int id;
+	
+	public int getRecommendationcount() {
+		return recommendationcount;
+	}
+	public void setRecommendationcount(int recommendationcount) {
+		this.recommendationcount = recommendationcount;
+	}
+	public boolean isCategorysingleplayer() {
+		return categorysingleplayer;
+	}
+	public void setCategorysingleplayer(boolean categorysingleplayer) {
+		this.categorysingleplayer = categorysingleplayer;
+	}
+	public boolean isCategorymultiplayer() {
+		return categorymultiplayer;
+	}
+	public void setCategorymultiplayer(boolean categorymultiplayer) {
+		this.categorymultiplayer = categorymultiplayer;
+	}
+	public boolean isCategorycoop() {
+		return categorycoop;
+	}
+	public void setCategorycoop(boolean categorycoop) {
+		this.categorycoop = categorycoop;
+	}
+	public boolean isCategorymmo() {
+		return categorymmo;
+	}
+	public void setCategorymmo(boolean categorymmo) {
+		this.categorymmo = categorymmo;
+	}
+	public boolean isCategoryvrsupport() {
+		return categoryvrsupport;
+	}
+	public void setCategoryvrsupport(boolean categoryvrsupport) {
+		this.categoryvrsupport = categoryvrsupport;
+	}
+	public boolean isGenreisindie() {
+		return genreisindie;
+	}
+	public void setGenreisindie(boolean genreisindie) {
+		this.genreisindie = genreisindie;
+	}
+	public boolean isGenreisaction() {
+		return genreisaction;
+	}
+	public void setGenreisaction(boolean genreisaction) {
+		this.genreisaction = genreisaction;
+	}
+	public boolean isGenreisadventure() {
+		return genreisadventure;
+	}
+	public void setGenreisadventure(boolean genreisadventure) {
+		this.genreisadventure = genreisadventure;
+	}
+	public boolean isGenreiscasual() {
+		return genreiscasual;
+	}
+	public void setGenreiscasual(boolean genreiscasual) {
+		this.genreiscasual = genreiscasual;
+	}
+	public boolean isGenreisstrategy() {
+		return genreisstrategy;
+	}
+	public void setGenreisstrategy(boolean genreisstrategy) {
+		this.genreisstrategy = genreisstrategy;
+	}
+	public boolean isGenreisrpg() {
+		return genreisrpg;
+	}
+	public void setGenreisrpg(boolean genreisrpg) {
+		this.genreisrpg = genreisrpg;
+	}
+	public boolean isGenreissimulation() {
+		return genreissimulation;
+	}
+	public void setGenreissimulation(boolean genreissimulation) {
+		this.genreissimulation = genreissimulation;
+	}
+	public boolean isGenreissports() {
+		return genreissports;
+	}
+	public void setGenreissports(boolean genreissports) {
+		this.genreissports = genreissports;
+	}
+	public boolean isGenreisracing() {
+		return genreisracing;
+	}
+	public void setGenreisracing(boolean genreisracing) {
+		this.genreisracing = genreisracing;
+	}
+	public boolean isGenreismassivelymultiplayer() {
+		return genreismassivelymultiplayer;
+	}
+	public void setGenreismassivelymultiplayer(boolean genreismassivelymultiplayer) {
+		this.genreismassivelymultiplayer = genreismassivelymultiplayer;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	
+	public boolean equals(Jogo j) {
+		int cont = 0;
+		if(cont == 0)
+		return true;
+		else {
+			return false;
+		}
+	}
+}
